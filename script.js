76059f7a414aa00b762e9f0b549d9be4347d0bdc
Sean Gombart <skgombart@gmail.com>
2022-10-25 23:51:09 -0700

    Finalize game function, add resetGame function

--- script.js
+++ script.js
@@ -16,24 +16,30 @@ function playRound(playerSelection, computerSelection) {
                 case 'rock':
                     return tie;
                 case 'paper':
+                    loses += 1
                     return lose;
                 case 'scissors':
+                    wins += 1
                     return win;
             }
         case 'paper':
             switch (computerSelection) {
                 case 'rock':
+                    wins += 1
                     return win;
                 case 'paper':
                     return tie;
                 case 'scissors':
+                    loses += 1
                     return lose;
             }
         case 'scissors':
             switch (computerSelection) {
                 case 'rock':
+                    loses += 1
                     return lose;
                 case 'paper':
+                    wins += 1
                     return win;
                 case 'scissors':
                     return tie;
@@ -42,12 +48,28 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
+    for ( ; wins + loses < 5; ) {
         let playerSelection = prompt("Choose rock, paper, or scisssors");
         console.log(playRound(playerSelection, getComputerChoice()));
-
-
+        console.log('Wins:', wins)
+        console.log('Loses:', loses)
     }
+    if (wins > 3) {
+        console.log('Congrats! You win!')
+        return resetGame()
+    } else {
+        console.log('You lose. Better luck next time.')
+        return resetGame()
+    }
+    
 }
 
+function resetGame() {
+    wins = 0
+    loses = 0
+    console.log(game())
+}
+
+let wins = 0
+let loses = 0
 console.log(game())
