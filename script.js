be718a11913c88b8d31febe1e5d50130b8015ec8
Sean Gombart <skgombart@gmail.com>
2022-10-26 00:08:01 -0700

    Add documetnation, fix 3-2 win bug

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// Randomly returns either rock, paper, or scissors as the computer's choice.
 function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() *  choices.length)]
 }
 
+// Plays a round of rock, paper, scissors. Follows all standard rules.
+// Displays the player's selection and computer's selection.
+// Takes the player's selection and computer's selection as input and returns a statement indicating a win, lose, or tie.
 function playRound(playerSelection, computerSelection) {
     let tie = `Tie game! ${playerSelection} ties with ${computerSelection}`;   
     let win = `You win! ${playerSelection} beats ${computerSelection}`;
@@ -47,29 +51,33 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+// Plays a game of rock, paper, scissors, which consists of 5 rounds. 
+// Displays round result statement, wins, and loses. Prompts the user to enter their choice for the round.
+// Returns resetGame function
 function game() {
     for ( ; wins + loses < 5; ) {
         let playerSelection = prompt("Choose rock, paper, or scisssors");
         console.log(playRound(playerSelection, getComputerChoice()));
-        console.log('Wins:', wins)
-        console.log('Loses:', loses)
+        console.log('Wins:', wins);
+        console.log('Loses:', loses);
     }
-    if (wins > 3) {
-        console.log('Congrats! You win!')
+    if (wins >= 3) {
+        console.log('Congrats! You win!');
         return resetGame()
     } else {
-        console.log('You lose. Better luck next time.')
+        console.log('You lose. Better luck next time.');
         return resetGame()
     }
     
 }
 
+// Resets the wins and loses. Calls game() to allow the player to play another game.
 function resetGame() {
-    wins = 0
-    loses = 0
+    wins = 0;
+    loses = 0;
     console.log(game())
 }
 
-let wins = 0
-let loses = 0
+let wins = 0;
+let loses = 0;
 console.log(game())
