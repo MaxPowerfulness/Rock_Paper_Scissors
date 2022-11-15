f3f9973de2e5a042dd987116a1b288d302c27763
Sean Gombart <skgombart@gmail.com>
2022-11-15 00:27:34 -0800

    Fix DOM manipulation line break issue, replaced alerts with screen text, add playAgain function.

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+// Global variables.
+let wins = 0;
+let loses = 0;
+let playerSelection = "";
+let gameText = document.querySelector('.gameText');
+
+
 // Randomly returns either rock, paper, or scissors as the computer's choice.
 function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() *  choices.length)]
 }
 
-// Plays a round of rock, paper, scissors. Follows all standard rules.
-// Displays the player's selection and computer's selection.
-// Takes the player's selection and computer's selection as input and returns a statement indicating a win, lose, or tie.
+/* 
+Plays a round of rock, paper, scissors. Follows all standard rules.
+Displays the player's selection and computer's selection.
+Takes the player's selection and computer's selection as input and returns a statement indicating a win, lose, or tie.
+*/
+
 function playRound(playerSelection, computerSelection) {
     let tie = `Tie game! ${playerSelection} ties with ${computerSelection}`;   
     let win = `You win! ${playerSelection} beats ${computerSelection}`;
@@ -18,6 +28,7 @@ function playRound(playerSelection, computerSelection) {
         case 'rock':
             switch (computerSelection) {
                 case 'rock':
+                    gameText.innerHTML = 'You trade hits';
                     break ;
                 case 'paper':
                     loses += 1;
@@ -33,6 +44,7 @@ function playRound(playerSelection, computerSelection) {
                     wins += 1;
                     break;
                 case 'paper':
+                    gameText.innerHTML = 'You trade hits';
                     break;
                 case 'scissors':
                     loses += 1;
@@ -48,6 +60,7 @@ function playRound(playerSelection, computerSelection) {
                     wins += 1;
                     break;
                 case 'scissors':
+                    gameText.innerHTML = 'You trade hits';
                     break;
             }
             break;
@@ -57,19 +70,17 @@ function playRound(playerSelection, computerSelection) {
     return;
 }
 
-// Checks to see if either the player or opponent has reached 3 wins. 
-// Displays the player's total wins and loses and resets the game.
+/* 
+Checks to see if either the player or opponent has reached 3 wins. 
+Displays the player's total wins and loses and resets the game.
+*/
 function checkGame() {
     if (wins >= 3) {
-        alert(`Wins: ${wins} 
-Loses: ${loses}
-Congrats! You win!`);
-        resetGame();
+        gameText.innerHTML = 'Congrats! You have successfully defended yourself from the pirates.';
+        playAgain();
     } else if (loses >= 3) {
-        alert(`Wins: ${wins} 
-Loses: ${loses}
-You lose. Better luck next time.`);
-        resetGame();
+        gameText.innerHTML = 'Your booty is plundered and ship is destoryed. Better luck next time.';
+        playAgain();
     }
 }
 
@@ -80,15 +91,25 @@ function displayScore() {
 
 // Resets the wins and loses. Calls game() to allow the player to play another game.
 function resetGame() {
+    gameText.innerHTML = `You look ahead at the looming ship as is gets closer and closer...<br> <br> You prepare the defenses`;
     wins = 0;
     loses = 0;
     displayScore();
 }
 
-// Global variables.
-let wins = 0;
-let loses = 0;
-let playerSelection = ""
+// Creates a button that allows the player to reset the game if they desire to play it again.
+function playAgain() {
+    const playAgain = document.createElement('button');
+    playAgain.innerHTML = 'Play Again';
+    playAgain.setAttribute('style', `font-size: 20px; border: none; border-radius: 20px;
+                           padding: 10px 18px; cursor: pointer`);
+    playAgain.addEventListener('click', function() {
+        resetGame();
+        document.body.removeChild(playAgain);
+    })
+    const choices = document.querySelector('.choices');
+    document.body.insertBefore(playAgain, choices);
+}
 
 
 // DOM manupulation
